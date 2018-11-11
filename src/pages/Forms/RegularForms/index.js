5cdab44ac3a1b3ef5970ca3245398fbe7cb68acd
Juliano Pereira Lima <julianodgtz@gmail.com>
2018-11-11 15:22:32 -0200

    Espero que dê bom

--- src/pages/Forms/RegularForms/index.js
+++ src/pages/Forms/RegularForms/index.js
@@ -1,65 +1,95 @@
-import React from 'react';
-import SpeechRecognition from 'react-speech-recognition'
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
+import SpeechRecognition from "react-speech-recognition";
+import StripedTable from "../../Tables/RegularTables/StripedTable";
 
 class StackedForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dados: [{ desc: "Dr Rey Atrasou" }]
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    var { dados } = this.state;
+    dados.push({ desc: this.input.value });
+    this.setState({ dados: dados });
+  }
+
   render() {
     const {
       submitting,
-      handleSubmit,
-      transcript,
       startListening,
       stopListening,
       listening,
+      transcript,
       match
     } = this.props;
 
     var dados = JSON.parse(match.params.dados);
-    console.log(dados);
-
     return (
       <div className="card">
         <div className="header">
           <h2>{dados.title}</h2>
           <br />
           <h4>{dados.desc}</h4>
-          <h6>De {moment(dados.start).format('DD/MM/YYYY')} até {moment(dados.end).format('DD/MM/YYYY')}</h6>
+          <h6>
+            De {moment(dados.start).format("DD/MM/YYYY")} até{" "}
+            {moment(dados.end).format("DD/MM/YYYY")}
+          </h6>
         </div>
 
         <div className="content">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label className="control-label">Registrar histórico</label>
-              <input
-                className="form-control"
-                name="historico"
-                type="text"
-                value={transcript}
-              />
-            </div>
+          <div className="form-group">
+            <label className="control-label">Registrar histórico</label>
+            <input
+              className="form-control"
+              name="historico"
+              type="text"
+              ref={(ref) => {this.input = ref}}
+              value={transcript}
+            />
+          </div>
 
-            <button
-              type="button"
-              className="btn btn-fill btn-info"
-              onClick={() => { listening ? stopListening() : startListening() }}
-              style={!listening ? {
-                backgroundColor: '#f03a37',
-                color: 'white',
-                border: 'none',
-                fontSize: '20px',
-                marginRight: '5px',
-              } :
-                {
-                  backgroundColor: '#f03a37',
-                  color: 'white',
-                  border: 'none',
-                  marginRight: '5px',
-                }}>
-              {listening ? 'Ouvindo...' : <i className="pe-7s-micro"></i>}
-            </button>
+          <button
+            type="button"
+            className="btn btn-fill btn-info"
+            onClick={() => {
+              listening ? stopListening() : startListening();
+            }}
+            style={
+              !listening
+                ? {
+                    backgroundColor: "#f03a37",
+                    color: "white",
+                    border: "none",
+                    fontSize: "20px",
+                    marginRight: "5px"
+                  }
+                : {
+                    backgroundColor: "#f03a37",
+                    color: "white",
+                    border: "none",
+                    marginRight: "5px"
+                  }
+            }
+          >
+            {listening ? "Ouvindo..." : <i className="pe-7s-micro" />}
+          </button>
 
-            <button type="submit" disable={listening} className="btn btn-fill btn-info" disabled={submitting}>Salvar</button>
-          </form>
+          <button
+            type="submit"
+            disable={listening}
+            className="btn btn-fill btn-info"
+            disabled={submitting}
+            onClick={this.handleClick}
+          >
+            Salvar
+          </button>
+          <StripedTable data={this.state.dados || []} filter={dados} />
         </div>
       </div>
     );
@@ -68,6 +98,6 @@ class StackedForm extends React.Component {
 
 const options = {
   autoStart: false
-}
+};
 
-export default SpeechRecognition(options)(StackedForm)
+export default SpeechRecognition(options)(StackedForm);
--- src/pages/Tables/RegularTables/StripedTable.js
+++ src/pages/Tables/RegularTables/StripedTable.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React from "react";
+import moment from "moment";
 
-const StripedTable = ({data}) => (
-  <div className="card">
-    <div className="header">
-      <h4 className="title">Striped Table with Hover</h4>
-      <p className="category">Here is a subtitle for this table</p>
-    </div>
-    <div className="content table-responsive table-full-width">
-      <table className="table table-hover table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Salary</th>
-            <th>Country</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>$ {item.salary}</td>
-              <td>{item.country}</td>
-              <td>{item.city}</td>
+const StripedTable = ({ data }) => {
+  return (
+    <div className="card">
+      <div className="header">
+        <h4 className="title">Historico</h4>
+      </div>
+      <div className="content table-responsive table-full-width">
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>Observacoes</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-
+          </thead>
+          <tbody>
+            {data.map(item => (
+              <tr>
+                <td>{item.desc}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default StripedTable;
